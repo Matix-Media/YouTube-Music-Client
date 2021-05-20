f3e81a8694d5f75f59f1354b7b07d03f4c574738
BigBrainAFK <tony.langhammer@yahoo.de>
2021-05-20 21:58:04 +0200

    Fixed dev menu opening on boot

--- src/main.js
+++ src/main.js
@@ -109,7 +109,7 @@ function createWindow() {
 		// Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36
 	});
 
-	win.webContents.openDevTools();
+	// win.webContents.openDevTools();
 
 	if (!config.continueWhereLeftOf) return;
 
